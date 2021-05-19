c3a8e41605a775c98b86e636d26dccd19540767e
matchifang <dmatchima@gmail.com>
2021-05-19 21:16:49 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FMProgressHUD
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import FMProgressHUDTests
+
+var tests = [XCTestCaseEntry]()
+tests += FMProgressHUDTests.allTests()
+XCTMain(tests)
